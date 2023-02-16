a226ae1f31bf732e2a43b49cf04c8842bab61fc9
Saliha <salihaoguz2011@gmail.com>
2023-02-16 21:53:45 +0000

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Card from "./components/Card";
+import Footer from "./components/Footer";
+import "./scss/App.scss";
+import data from "./data";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Card data={data} />
+      <Footer />
+    </>
   );
 }
 
